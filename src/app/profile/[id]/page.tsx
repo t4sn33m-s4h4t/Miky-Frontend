c16c067c306d4d4f4cbbd20a9b3b5f7bf3eec12c
t4sn33m-s4h4t <mdsahat6397@gmail.com>
2025-03-18 13:52:41 +0600

    fixed params

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,7 +1,7 @@
 import Profile from '@/components/Profile/Profile'
 import React from 'react'
 
-const ProfilePage = async ({params}:{params:{id:string}}) => {
+const ProfilePage = async ({params}:{params:Promise<{id:string}> }) => {
    const id = (await params).id
   return (
     <>
